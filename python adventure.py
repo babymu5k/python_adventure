b87ea08898055a61d7bf6e2f31a142909c58d2dd
superpythonguy <95881676+superpythonguy@users.noreply.github.com>
2021-12-10 17:44:04 +0000

    Update python adventure.py

--- python adventure.py
+++ python adventure.py
@@ -1,7 +1,10 @@
-import sys
+mport sys
 import time
 import random
 global hp
+
+quest=0
+gold_coin=0
 inv = "nothing"
 hp = 100
 if hp <= 0:
@@ -56,15 +59,45 @@ if a == "attack":
 
 d = input("do you want to goto the" " village " "or continue in the" " mountain  ")
 if d == "village":
+    time.sleep(1)
     print("you went the village and drink a healing potion ")
     time.sleep(1)
     print("!50hp extra!")
     hp = hp + 50
     print(hp, "hp")
-    print("you must solve a maths problem to continue")
+
 elif d == "mountain":
     print("you continue in the mountains and trip and lose 50hp")
     hp = hp - 50
     print("you have ", hp ,"hp")
+    sys.exit()
 
+print("in the village you find a box")
+time.sleep(1)
+print("it contains a torch,string and a rubber band")
+inv="torch","string","rubber band" #added to inv
 
+print(inv)
+quest= quest+1
+print(name,"so far you have completed",quest,"quests" )
+print("a villager appears he wants a rubber band there is 33% chance you will find it")
+r=(random.choice(inv))
+print("you found a",r)
+if r == "rubber band":
+    time.sleep(1)
+    print("the villager accepted your request recieve 1 gold coin")
+    print("*villager:thank you ",name)
+    print("you have",gold_coin,"gold coins ")
+else:
+    print("you could not find a rubber band")
+
+checkinv=input("do you want to check your inventory " "yes " "no ")
+if checkinv=="yes":
+    print("you have", inv)
+else:
+    print("ok.")
+checkhp=input("do you want to check hp " "yes " "no ")
+if checkhp=="yes":
+    print(hp,"hp")
+else:
+    print("ok then")
